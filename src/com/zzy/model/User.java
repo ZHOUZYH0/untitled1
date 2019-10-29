098f69485f5d4779fd60a0cbd5ae801d3851378c
ZHOUZYH0 <zychina66@163.com>
2019-10-29 12:52:50 +0800

    user toString

--- src/com/zzy/model/User.java
+++ src/com/zzy/model/User.java
@@ -10,6 +10,15 @@ public class User {
 
     }
 
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+
     public Integer getId() {
         return id;
     }
